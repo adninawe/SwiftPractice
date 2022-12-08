c8ea4da33bccca3a2431e79fb6dc0b37a3a3c0f1
Amit D. Ninawe <amitdninawe@gmail.com>
2022-12-08 19:32:20 +0530

    Project added

---
+++ UIPractice/UIPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIPractice
+//
+//  Created by Amit Ninawe on 08/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
